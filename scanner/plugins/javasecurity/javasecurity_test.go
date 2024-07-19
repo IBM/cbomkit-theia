d2fc02a309d57da593eff54c2c686b1b31af4e11
Bengt Wegner <Jochen.Wegner@ibm.com>
2024-07-19 14:46:19 +0200

    Test(#14): Added unit tests

---
+++ scanner/plugins/javasecurity/javasecurity_test.go
@@ -0,0 +1,72 @@
+package javasecurity
+
+import (
+	"testing"
+
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+)
+
+func Test_getJDKPath(t *testing.T) {
+	type args struct {
+		dockerConfig v1.Config
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantValue string
+		wantOk    bool
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				dockerConfig: v1.Config{
+					Cmd: []string{"java", "-jar", "app.jar"},
+				},
+			},
+			wantValue: "",
+			wantOk:    false,
+		},
+		{
+			name: "Test 2",
+			args: args{
+				dockerConfig: v1.Config{
+					Cmd: []string{"java", "-jar", "app.jar"},
+					Env: []string{"JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.292.b10-0.el8_4.x86_64"},
+				},
+			},
+			wantValue: "/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.292.b10-0.el8_4.x86_64",
+			wantOk:    true,
+		},
+		{
+			name: "Test 3",
+			args: args{
+				dockerConfig: v1.Config{
+					Cmd: []string{"/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.292.b10-0.el8_4.x86_64/bin/java", "-jar", "app.jar"},
+				},
+			},
+			wantValue: "/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.292.b10-0.el8_4.x86_64",
+			wantOk:    true,
+		},
+		{
+			name: "Test 4",
+			args: args{
+				dockerConfig: v1.Config{
+					Entrypoint: []string{"/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.292.b10-0.el8_4.x86_64/bin/java", "-jar", "app.jar"},
+				},
+			},
+			wantValue: "/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.292.b10-0.el8_4.x86_64",
+			wantOk:    true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotValue, gotOk := getJDKPath(tt.args.dockerConfig)
+			if gotValue != tt.wantValue {
+				t.Errorf("getJDKPathFromRunCommand() gotValue = %v, want %v", gotValue, tt.wantValue)
+			}
+			if gotOk != tt.wantOk {
+				t.Errorf("getJDKPathFromRunCommand() gotOk = %v, want %v", gotOk, tt.wantOk)
+			}
+		})
+	}
+}
